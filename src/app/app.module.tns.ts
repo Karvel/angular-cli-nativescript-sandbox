fb46b02137eab0d206a5767243706b4efd498841
Elanna Grossman <elanna.grossman@gmail.com>
2019-04-28 14:22:54 -0700

    style(app): apply style fixes

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -1,9 +1,9 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { NativeScriptModule }         from 'nativescript-angular/nativescript.module';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule }           from './app-routing.module';
+import { AppComponent }               from './app.component';
+import { HomeComponent }              from './home/home.component';
 
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
@@ -13,16 +13,16 @@ import { HomeComponent } from './home/home.component';
 // import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
-  imports: [
-    NativeScriptModule,
-    AppRoutingModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-  schemas: [NO_ERRORS_SCHEMA]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+	],
+		imports: [
+		NativeScriptModule,
+		AppRoutingModule,
+	],
+		providers: [],
+		bootstrap: [AppComponent],
+		schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  title = 'angular-cli-nativescript-sandbox';
+	title = 'angular-cli-nativescript-sandbox';
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 }
