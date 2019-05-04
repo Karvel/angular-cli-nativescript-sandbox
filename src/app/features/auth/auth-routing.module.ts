d2620e3d085d79b61406fbec722267f5630173ec
Elanna Grossman <elanna.grossman@gmail.com>
2019-05-03 20:51:28 -0700

    feat(auth-routing): add web-only `AuthRoutingModule`

---
+++ src/app/features/auth/auth-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule }              from '@angular/core';
+import { RouterModule, Routes }  from '@angular/router';
+
+import { LoginComponent }        from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
+const routes: Routes = [
+	{
+		path: '',
+		redirectTo: '/login',
+		pathMatch: 'full',
+	},
+	{
+		path: 'login',
+		component: LoginComponent,
+		data: { title: 'Login' },
+	},
+	{
+		path: 'forgot-password',
+		component: ForgotPasswordComponent,
+		data: { title: 'Forgot Password' },
+	},
+];
+
+@NgModule({
+  imports : [ RouterModule.forChild(routes) ],
+  exports : [ RouterModule ],
+})
+export class AuthRoutingModule {}
+
+export const routedComponents = [
+	LoginComponent,
+	ForgotPasswordComponent,
+];
