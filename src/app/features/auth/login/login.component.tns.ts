5a7309f0c9e1cda6397f2da849ef27959724b8b2
Elanna Grossman <elanna.grossman@gmail.com>
2019-05-03 20:45:44 -0700

    feat(login): add web/native hybrid `LoginComponent`

---
+++ src/app/features/auth/login/login.component.tns.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+	selector        : 'app-login',
+	templateUrl     : './login.component.html',
+	styleUrls       : ['./login.component.scss'],
+	changeDetection : ChangeDetectionStrategy.OnPush,
+})
+export class LoginComponent implements OnInit {
+
+	constructor() { }
+
+	public ngOnInit(): void {
+		this.login();
+	}
+
+	private login(): void {
+		console.log('mobile login');
+	}
+}
