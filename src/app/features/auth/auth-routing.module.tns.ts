2d8e4d5f379f81f71a3c845481e8e06e4beec76e
Elanna Grossman <elanna.grossman@gmail.com>
2019-05-03 20:51:18 -0700

    feat(auth-routing): add native-only `AuthRoutingModule`

---
+++ src/app/features/auth/auth-routing.module.tns.ts
@@ -0,0 +1,36 @@
+import { NgModule }                 from '@angular/core';
+import { Routes }                   from '@angular/router';
+
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
+
+import { LoginComponent }          from './login/login.component.tns';
+import { ResetPasswordComponent }  from './reset-password/reset-password.component.tns';
+
+const routes: Routes = [
+	{
+		path: '',
+		redirectTo: '/login',
+		pathMatch: 'full',
+	},
+	{
+		path: 'login',
+		component: LoginComponent,
+		data: { title: 'Login' },
+	},
+	{
+		path: 'reset-password',
+		component: ResetPasswordComponent,
+		data: { title: 'Reset Password' },
+	},
+];
+
+@NgModule({
+	imports: [NativeScriptRouterModule.forRoot(routes)],
+	exports: [NativeScriptRouterModule],
+})
+export class AuthRoutingModule { }
+
+export const routedComponents = [
+	LoginComponent,
+	ResetPasswordComponent,
+];
