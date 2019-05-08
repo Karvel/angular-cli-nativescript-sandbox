464a02146c7ab8cdb36bde1de13060eaa8e138c6
Elanna Grossman <elanna.grossman@gmail.com>
2019-05-07 17:07:47 -0700

    feat(not-found): use label native component, tap event

--- src/app/features/not-found/not-found.component.ts
+++ src/app/features/not-found/not-found.component.ts
@@ -1,9 +1,21 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+	Component,
+	ChangeDetectionStrategy,
+}                         from '@angular/core';
+import { RoutingService } from '../../infrastructure/core/services/routing.service';
 
 @Component({
-  selector        : 'app-not-found',
-  templateUrl     : './not-found.component.html',
-  styleUrls       : ['./not-found.component.scss'],
-  changeDetection : ChangeDetectionStrategy.OnPush,
+	selector        : 'app-not-found',
+	templateUrl     : './not-found.component.html',
+	styleUrls       : ['./not-found.component.scss'],
+	changeDetection : ChangeDetectionStrategy.OnPush,
 })
-export class NotFoundComponent { }
+export class NotFoundComponent {
+	constructor(
+		private routingService: RoutingService,
+	) { }
+
+	public returnToRoot(): void {
+		this.routingService.navigateByURL('/');
+	}
+}
