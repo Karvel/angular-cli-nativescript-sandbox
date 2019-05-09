f3f1e2a0eb392ba65e8546fa577c255f175c4281
Elanna Grossman <elanna.grossman@gmail.com>
2019-05-09 13:33:38 -0700

    feat(home): remove placeholder content, add link button

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component }      from '@angular/core';
+
+import { RoutingService } from '~/app/infrastructure/core/services/routing.service';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-	title = 'angular-cli-nativescript-sandbox';
+export class HomeComponent {
+	public title = 'angular-cli-nativescript-sandbox';
 
-	constructor() { }
+	constructor(
+		private routingService: RoutingService,
+	) { }
 
-	ngOnInit() { }
+	public goToLogin(): void {
+		this.routingService.navigateByURL('/auth/login');
+	}
 }
