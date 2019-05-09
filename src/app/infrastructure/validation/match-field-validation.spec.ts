0e8c63b7ef571b212e0891226ff1e367ca15af7a
Elanna Grossman <elanna.grossman@gmail.com>
2019-05-09 14:02:11 -0700

    test(match-field-validation): rename parameters and variables for clarity

--- src/app/infrastructure/validation/match-field-validation.spec.ts
+++ src/app/infrastructure/validation/match-field-validation.spec.ts
@@ -8,35 +8,35 @@ import { MatchFieldValidation }   from './match-field-validation';
 !environment.testUnit
 ? Logger.log('Unit skipped')
 : describe('[Unit] MatchFieldValidation validFieldMatch()', () => {
-	const confirmEmailValidator = MatchFieldValidation.validFieldMatch('emailControl', 'confirmEmailControl');
+	const confirmEmailValidator = MatchFieldValidation.validFieldMatch('controlName', 'confirmControlName');
 	const form: FormGroup = new FormGroup({
-		emailControl: new FormControl(''),
-		confirmEmailControl: new FormControl(''),
+		controlName: new FormControl(''),
+		confirmControlName: new FormControl(''),
 	});
-	const emailControl = form.get('emailControl');
-	const confirmEmailControl = form.get('confirmEmailControl');
+	const controlName = form.get('controlName');
+	const confirmControlName = form.get('confirmControlName');
 
 	it(`should set control error as { 'confirmFieldRequired': 'Confirm Email is required.' } when value is an empty string`, () => {
-		emailControl.setValue('');
-		confirmEmailControl.setValue('');
+		controlName.setValue('');
+		confirmControlName.setValue('');
 		confirmEmailValidator(form);
 		const expectedValue = { 'confirmFieldRequired': 'Confirm Email is required.' };
-		expect(confirmEmailControl.errors).toEqual(expectedValue);
+		expect(confirmControlName.errors).toEqual(expectedValue);
 	});
 
 	it(`should set control error as { 'fieldsMismatched': 'Email fields do not match.' } when values do not match`, () => {
-		emailControl.setValue('test@test.com');
-		confirmEmailControl.setValue('test@test.co');
+		controlName.setValue('test@test.com');
+		confirmControlName.setValue('test@test.co');
 		confirmEmailValidator(form);
 		const expectedValue = { 'fieldsMismatched': 'Email fields do not match.' };
-		expect(confirmEmailControl.errors).toEqual(expectedValue);
+		expect(confirmControlName.errors).toEqual(expectedValue);
 	});
 
 	it(`should set control error as null when values match`, () => {
-		emailControl.setValue('test@test.com');
-		confirmEmailControl.setValue('test@test.com');
+		controlName.setValue('test@test.com');
+		confirmControlName.setValue('test@test.com');
 		confirmEmailValidator(form);
-		expect(emailControl.errors).toEqual(null);
-		expect(confirmEmailControl.errors).toEqual(null);
+		expect(controlName.errors).toEqual(null);
+		expect(confirmControlName.errors).toEqual(null);
 	});
 });
