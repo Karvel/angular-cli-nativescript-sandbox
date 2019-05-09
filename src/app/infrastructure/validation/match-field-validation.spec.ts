150217d69ba5a385f61d76eda1fffab49aa8927a
Elanna Grossman <elanna.grossman@gmail.com>
2019-05-09 14:06:10 -0700

    test(match-field-validation): add tests for when field name param exists

--- src/app/infrastructure/validation/match-field-validation.spec.ts
+++ src/app/infrastructure/validation/match-field-validation.spec.ts
@@ -40,3 +40,31 @@ import { MatchFieldValidation }   from './match-field-validation';
 		expect(confirmControlName.errors).toEqual(null);
 	});
 });
+
+!environment.testUnit
+? Logger.log('Unit skipped')
+: describe('[Unit] MatchFieldValidation validFieldMatch() parameter field name', () => {
+	const matchFieldValidator = MatchFieldValidation.validFieldMatch('controlName', 'confirmControlName', 'Password');
+	const form: FormGroup = new FormGroup({
+		controlName: new FormControl(''),
+		confirmControlName: new FormControl(''),
+	});
+	const controlName = form.get('controlName');
+	const confirmControlName = form.get('confirmControlName');
+
+	it(`should set control error as { 'confirmFieldRequired': 'Confirm Password is required.' } when value is an empty string`, () => {
+		controlName.setValue('');
+		confirmControlName.setValue('');
+		matchFieldValidator(form);
+		const expectedValue = { 'confirmFieldRequired': 'Confirm Password is required.' };
+		expect(confirmControlName.errors).toEqual(expectedValue);
+	});
+
+	it(`should set control error as { 'fieldsMismatched': 'Password fields do not match.' } when values do not match`, () => {
+		controlName.setValue('test@test.com');
+		confirmControlName.setValue('test@test.co');
+		matchFieldValidator(form);
+		const expectedValue = { 'fieldsMismatched': 'Password fields do not match.' };
+		expect(confirmControlName.errors).toEqual(expectedValue);
+	});
+});
