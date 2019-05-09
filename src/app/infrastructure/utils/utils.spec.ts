c8336eed934ab2d60bfd648ba9ce93d1faa502a9
Elanna Grossman <elanna.grossman@gmail.com>
2019-05-09 15:24:47 -0700

    test(utils): add tests

---
+++ src/app/infrastructure/utils/utils.spec.ts
@@ -0,0 +1,25 @@
+import { environment } from '../../../environments/environment.test';
+import { Logger }      from './logger';
+import { Utils }       from './utils';
+
+!environment.testUnit
+? Logger.log('Unit skipped')
+: describe('[Unit] Utils compareArrays', () => {
+	it('should be false when arrays of different lengths are passed', () => {
+		const array1: number[] = [1, 2];
+		const array2DifferentLength: number[] = [1, 2, 3];
+		expect(Utils.compareArrays(array1, array2DifferentLength)).toEqual(false);
+	});
+
+	it('should be true when same-length arrays with the same values are passed', () => {
+		const array1: number[] = [3, 1, 2];
+		const array2: number[] = [1, 2, 3];
+		expect(Utils.compareArrays(array1, array2)).toEqual(true);
+	});
+
+	it('should be false when same-length arrays with different values are passed', () => {
+		const array1: number[] = [3, 1, 2];
+		const array2: number[] = [1, 2, 5];
+		expect(Utils.compareArrays(array1, array2)).toEqual(false);
+	});
+});
