6e7935a3217f238e80e22e0f10c781d3f93c3db4
Elanna Grossman <elanna.grossman@gmail.com>
2019-05-10 11:21:59 -0700

    fix(controls): split controls import to remove web-only `GridComponet`

---
+++ src/app/infrastructure/shared/controls/index.tns.ts
@@ -0,0 +1,7 @@
+import { EmailControlComponent }  from './email-control/email-control.component';
+import { RequiredInputComponent } from './required-input/required-input.component';
+
+export const controls = [
+	EmailControlComponent,
+	RequiredInputComponent,
+];
